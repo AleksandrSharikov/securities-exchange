2487f3933905bac6843efe7e5a63e69c0681295e
Pavel Sablin <slipperlama@gmail.com>
2023-02-27 18:33:58 +0300

    Small change according to SonarLint

--- quotes-api/src/main/java/com/exchangeinformant/services/StockService.java
+++ quotes-api/src/main/java/com/exchangeinformant/services/StockService.java
@@ -1,9 +1,6 @@
 package com.exchangeinformant.services;
 
 import com.exchangeinformant.model.Stock;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 /**
