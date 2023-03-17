446bd17e54b06cf0d0b0c165ad3e3bc9efe79fe1
FyodorProgrammer <669396@gmail.com>
2023-03-18 01:52:12 +0800

    KATA-TEAM-159: объединить с КАТА-ТЕАМ-157, не запускается через докер subscription
    Были бины с одинаковыми именами, что не позволяло спрингу создать необходимые бины. Решил проблему переименованием конфликтующих бинов
    
    Так же один из классов был без аннотаций @Entity и @Id, что не позволяло спрингу связать его с БД

--- subscription/src/main/java/com/exchangeinformant/subscription/config/MessageFromPayment.java
+++ subscription/src/main/java/com/exchangeinformant/subscription/config/MessageFromPayment.java
@@ -2,7 +2,7 @@ package com.exchangeinformant.subscription.config;
 
 
 import com.exchangeinformant.subscription.service.LogicForPaymentProofService;
-import com.exchangeinformant.subscription.util.rabbitMQ.PaymentProof;
+import com.exchangeinformant.subscription.util.rabbitMQ.PaymentProofService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,10 +17,10 @@ public class MessageFromPayment {
     LogicForPaymentProofService logicForPaymentProofService;
 
     @Bean
-    public Consumer<Message<PaymentProof>> getPaymentProof() {
+    public Consumer<Message<PaymentProofService>> getPaymentProof() {
 
         return (paymentProof) -> {
-            PaymentProof paymentProofFromMessage = new PaymentProof();
+            PaymentProofService paymentProofFromMessage = new PaymentProofService();
 
             paymentProofFromMessage.setSubscription_id(paymentProof.getPayload().getSubscription_id());
             paymentProofFromMessage.setPay_amount(paymentProof.getPayload().getPay_amount());
--- subscription/src/main/java/com/exchangeinformant/subscription/service/LogicForPaymentProofService.java
+++ subscription/src/main/java/com/exchangeinformant/subscription/service/LogicForPaymentProofService.java
@@ -1,18 +1,11 @@
 package com.exchangeinformant.subscription.service;
 
 import com.exchangeinformant.subscription.dto.SubscriptionDTO;
-import com.exchangeinformant.subscription.model.Subscription;
 import com.exchangeinformant.subscription.model.Tariff;
-import com.exchangeinformant.subscription.repository.SubscriptionRepository;
 import com.exchangeinformant.subscription.util.Timer.TimerSubscriptionEnd;
 import com.exchangeinformant.subscription.util.enums.Status;
 import com.exchangeinformant.subscription.util.error.MessageError;
-import com.exchangeinformant.subscription.util.rabbitMQ.PaymentProof;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-
-
-import java.time.LocalDateTime;
+import com.exchangeinformant.subscription.util.rabbitMQ.PaymentProofService;
 
 public class LogicForPaymentProofService {
     SubscriptionServiceImpl subscriptionServiceImpl;
@@ -21,18 +14,18 @@ public class LogicForPaymentProofService {
     MessageError messageError;
     Tariff tariff;
 
-    public void logicForPayment(PaymentProof paymentProof) {
-        subscriptionDTO = subscriptionServiceImpl.getSubscription(paymentProof.getSubscription_id());
-        if (tariff.getTariffCost() != Integer.parseInt(paymentProof.getPay_amount())) {
+    public void logicForPayment(PaymentProofService paymentProofService) {
+        subscriptionDTO = subscriptionServiceImpl.getSubscription(paymentProofService.getSubscription_id());
+        if (tariff.getTariffCost() != Integer.parseInt(paymentProofService.getPay_amount())) {
             if (subscriptionDTO.getStatus() != Status.ACTIVE) {
                 subscriptionDTO.setStatus(Status.PAYMENT_ERROR);
                 subscriptionDTO.setErrorDescription(messageError.createErrorDescription("Оплата не соответствует заданному тарифу и подписка не активна"));
                 subscriptionServiceImpl.updateSubscription(subscriptionDTO);
             }
         } else {
-            if (tariff.getTariffCost() == Integer.parseInt(paymentProof.getPay_amount())) {
+            if (tariff.getTariffCost() == Integer.parseInt(paymentProofService.getPay_amount())) {
                 subscriptionDTO.setStatus(Status.ACTIVE);
-                subscriptionDTO.setPrice(Integer.parseInt(paymentProof.getPay_amount()));
+                subscriptionDTO.setPrice(Integer.parseInt(paymentProofService.getPay_amount()));
                 subscriptionDTO.setExpiresAt(timerSubscriptionEnd.methodOfExpiresSub(subscriptionDTO.getPrice()));
                 subscriptionServiceImpl.updateSubscription(subscriptionDTO);
             } else {
