cbef0a2c3cbb7f81f2d4bfd3cc88d866a5fb2ea8
Westalex777 <westalex777@yandex.ru>
2023-04-04 12:54:06 +0600

    commit-review

---
+++ notifier-test/src/main/java/com/example/notifiertest/config/ConfigEventSource.java
@@ -0,0 +1,28 @@
+package com.example.notifiertest.config;
+
+import com.example.notifiertest.model.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.*;
+
+@Configuration
+public class ConfigEventSource {
+
+    @Bean("json-event-source")
+    public LinkedList<String> createLinkedListJSON() {
+        return new LinkedList<>();
+    }
+
+    @Bean("Users-for-test")
+    public List<User> createListUsers() {
+        return new ArrayList<>(Arrays.asList(
+                new User(1, "westalex777@yandex.ru", "+7 777 777 77 01"),
+//                new User(2, "Dla_del@mail.ru", "+7 777 777 77 02"),
+//                new User(3, "alex_merser_00@internet.ru", "+7 777 777 77 03"),
+                new User(2, "email_4_@email.ru", "+7 777 777 77 02"),
+                new User(3, "email_4_@email.ru", "+7 777 777 77 03"),
+                new User(4, "email_4_@email.ru", "+7 777 777 77 04"),
+                new User(5, "email_5_@email.ru", "+7 777 777 77 05")));
+    }
+}
---
+++ notifier-test/src/test/java/com/example/notifiertest/NotifierTestApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.example.notifiertest;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class NotifierTestApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
